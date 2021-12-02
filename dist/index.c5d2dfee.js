cffbebf5fd4a8275c83a7fbaa813e7da69aacb2b
mln35 <mouhametlatyr@gmail.com>
2021-12-02 02:31:57 +0000

    initial commit

--- dist/index.c5d2dfee.js
+++ dist/index.c5d2dfee.js
@@ -2,11 +2,9 @@ accounts = [];
 count = 0;
 trans = [];
 t_counts = 0;
-
-
-renderTransaction = (list) => {
+renderTransaction = (list)=>{
     table = document.querySelector('#table');
-    if(list && list.length > 0){
+    if (list && list.length > 0) {
         tbody = document.querySelector('#tbody');
         let num = 0;
         table.innerHTML = '';
@@ -22,27 +20,23 @@ renderTransaction = (list) => {
             code_td.innerHTML = l.code;
             label_td.innerHTML = l.label;
             class_td.innerHTML = l.class;
-
             tr.appendChild(code_td);
             tr.appendChild(label_td);
             tr.appendChild(class_td);
-            if(num%2===0){tr.classList.add("even-row");}
+            if (num % 2 === 0) tr.classList.add("even-row");
             num++;
             table.appendChild(tr);
         }
-    }else{
+    } else {
         // container = document.querySelector('.container');
         table.innerHTML = '';
         empty = document.querySelector('#empty');
         empty.innerHTML = 'No account saved yet';
-        
     }
-
-}
-
-render = (list) => {
+};
+render = (list)=>{
     table = document.querySelector('#table');
-    if(list && list.length > 0){
+    if (list && list.length > 0) {
         tbody = document.querySelector('#tbody');
         let num = 0;
         table.innerHTML = '';
@@ -54,72 +48,66 @@ render = (list) => {
             code_td.innerHTML = l.code;
             label_td.innerHTML = l.label;
             class_td.innerHTML = l.class;
-
             tr.appendChild(code_td);
             tr.appendChild(label_td);
             tr.appendChild(class_td);
-            if(num%2===0){tr.classList.add("even-row");}
+            if (num % 2 === 0) tr.classList.add("even-row");
             num++;
             table.appendChild(tr);
         }
-    }else{
+    } else {
         // container = document.querySelector('.container');
         table.innerHTML = '';
         empty = document.querySelector('#empty');
         empty.innerHTML = 'No account saved yet';
-        
     }
-
-}
-
+};
 // fs = require('fs');
-add = () => {
+add = ()=>{
     console.log('add');
     // table = document.querySelector('.table');
     let _code = document.querySelector('.id-acount').value;
     let _label = document.querySelector('.account-name').value;
     let _class = document.querySelector('.class').value;
-    console.log('code: ',_code, 'label: ',_label,'class: ', _class)
-    
-    addAccount(_code, _label, _class)
+    console.log('code: ', _code, 'label: ', _label, 'class: ', _class);
+    addAccount(_code, _label, _class);
     render(accounts);
-}
-
-addAccount = (_code,_label,_class) =>{
-    accounts.push({code:_code, label:_label, class:_class})
-    localStorage.setItem('count',++count);
-    localStorage.setItem('code '+count,_code);
-    localStorage.setItem('label '+count,_label);
-    localStorage.setItem('class '+count,_class);
+};
+addAccount = (_code, _label, _class)=>{
+    accounts.push({
+        code: _code,
+        label: _label,
+        class: _class
+    });
+    localStorage.setItem('count', ++count);
+    localStorage.setItem('code ' + count, _code);
+    localStorage.setItem('label ' + count, _label);
+    localStorage.setItem('class ' + count, _class);
     console.table(accounts);
-}
-
-getAllFromLocalStorage = () => {
+};
+getAllFromLocalStorage = ()=>{
     count = localStorage.getItem('count');
-    if(count)
-    {
-        for(i=1; i<=count; i++){
-            let cd = localStorage.getItem('code '+i);
-            let lb = localStorage.getItem('label '+i);
-            let cl = localStorage.getItem('class '+i);
-            accounts.push({code:cd, label:lb, class:cl});
+    if (count) {
+        for(i = 1; i <= count; i++){
+            let cd = localStorage.getItem('code ' + i);
+            let lb = localStorage.getItem('label ' + i);
+            let cl = localStorage.getItem('class ' + i);
+            accounts.push({
+                code: cd,
+                label: lb,
+                class: cl
+            });
         }
         console.log(count);
-        console.table(accounts)
-    }
-    else {
-        count = 0;
-    }
-}
-
-
-clearAll = () => {
+        console.table(accounts);
+    } else count = 0;
+};
+clearAll = ()=>{
     count = localStorage.getItem('count');
-    if(count) localStorage.clear();
+    if (count) localStorage.clear();
     accounts = [];
     render(accounts);
-}
-
+};
 // load =(url) => {
 //     const contentDiv = document.querySelector(".content");
 //     fetch('./logs.html').then((data) => {
@@ -128,10 +116,9 @@ clearAll = () => {
 //     });
 //     // contentDiv.innerHTML = await fetchHtmlAsText("home.html");
 // }
-addTransaction =()=>{
-
-}
-
-
+addTransaction = ()=>{
+};
 getAllFromLocalStorage();
 render(accounts);
+
+//# sourceMappingURL=index.c5d2dfee.js.map
